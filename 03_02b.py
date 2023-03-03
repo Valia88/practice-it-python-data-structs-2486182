a8b7e70d71c145127a709f266469c9531f4b0d0f
Yvonne <46846918+Valia88@users.noreply.github.com>
2023-03-03 12:05:34 +0000

    update

--- 03_02b.py
+++ 03_02b.py
@@ -1,9 +1,20 @@
+from collections import namedtuple
+from csv import reader
+
 
 def main():
-    #add code here
-    #read data/Customer.csv
-    #Create workable objects with each line
+    # add code here
+    # read data/Customer.csv
+    with open("data/Customer.csv", "r") as open_csv:
+        read = reader(open_csv)
+        Person = namedtuple("person", next(read))
+
+        for line in read:
+            person = Person(*line)
+            print(person)
+    # Create workable objects with each line
     return
+
 
 if __name__ == "__main__":
     main()
